9d3845c0d4236e4a0acdf8d81f134191bf06c639
Kamil Pękala <kamilkp@gmail.com>
2016-09-04 21:53:50 +0200

    fixed syntax error

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf dist
 mkdir dist
 
-(echo "(function(){"; cat src/fp.js | ./node_modules/.bin/babel --presets babel-preset-es2015; echo "}();") > dist/fp.js
-echo "(function(){`cat src/fp.js | ./node_modules/.bin/babel --presets babel-preset-es2015 | ./node_modules/.bin/uglifyjs`}();" > dist/fp.min.js
+(echo "(function(){"; cat src/fp.js | ./node_modules/.bin/babel --presets babel-preset-es2015; echo "})();") > dist/fp.js
+echo "(function(){`cat src/fp.js | ./node_modules/.bin/babel --presets babel-preset-es2015 | ./node_modules/.bin/uglifyjs`})();" > dist/fp.min.js
--- dist/fp.js
+++ dist/fp.js
@@ -108,4 +108,4 @@ if (typeof module !== 'undefined' && module.exports) {
   window.fp = fp;
 }
 
-}();
+})();
